e544883fb01ae2cb963e327642bcefa56edecbd4
SYdney-Go <ksy960809@gmail.com>
2021-09-18 16:12:39 +0900

    20210918_1612
    카테고리 페이지 만들기
    만들기 쉽지 않다...

--- blog/tests.py
+++ blog/tests.py
@@ -63,7 +63,7 @@ class TestView(TestCase):
         # 만약 카테고리 항목이 있다면, 각 카테고리의 이름과 수가 text 내용과 일치하는지 확인하기
         self.assertIn(f'{self.category_programming.name} ({self.category_programming.post_set.count()})', categories_card.text)
         self.assertIn(f'{self.category_music.name} ({self.category_music.post_set.count()})', categories_card.text)
-        self.assertIn(f'미분류 (1)', categories_card.text)
+        self.assertIn(f'미분류', categories_card.text)
 
     def test_post_list(self):
         # 포스트가 있는 경우
@@ -96,7 +96,7 @@ class TestView(TestCase):
         self.assertIn('미분류', post_003_card.text)
 
         self.assertIn(self.user_trump.username.upper(), main_area.text)
-        self.assertIn(self.user_obama.username.uppser(), main_area.text)
+        self.assertIn(self.user_obama.username.upper(), main_area.text)
 
         # 포스트가 없는 경우
         # 모든 포스트 삭제 명령 후 포스트 카운트가 0이 맞는지 확인하고 blog url 연결 확인하기
@@ -106,7 +106,7 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
 
         # 게시글이 없다는 명령어가 제대로 떠있는지 확인하기
-        main_area = soup.find('div', id='main_area')
+        main_area = soup.find('div', id='main-area')
         self.assertIn('아직 게시물이 없습니다', main_area.text)
 
     def test_post_detail(self):
@@ -117,23 +117,26 @@ class TestView(TestCase):
             author=self.user_trump,
         )
         # 1.2 그 포스트의 url은 '/blog/1/'이다
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1')
+        self.assertEqual(self.post_001.get_absolute_url(), '/blog/1')
         
         # 2 첫번째 포스트의 상세 페이지 테스트
         # 2.1 첫 번째 포스트의 url로 접근하면 정상적으로 작동한다.
-        response = self.client.get(post_001.get_absolute_url(), follow=True)
+        response = self.client.get(self.post_001.get_absolute_url(), follow=True)
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')       
         # 2.2 포스트 목록 페이지와 똑같은 네비게이션 바가 있다.
         self.navbar_test(soup)
+        self.category_card_test(soup)
+
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어있다.
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(self.post_001.title, soup.title.text)
         # 2.4 첫 번째 포스트의 제목이 포스트 영역에 있다.
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(self.post_001.title, post_area.text)
+        self.assertIn(self.category_programming.name, post_area.text)
         # 2.5 첫 번째 포스트의 작성자가 포스트 영역에 있다.
         self.assertIn(self.user_trump.username.upper(), post_area.text)
         # 2.6 첫 번째 포스트의 내용이 포스트 영역에 있다.
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
 
